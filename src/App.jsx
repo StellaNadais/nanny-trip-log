b1fc2f8fadb230112c2cb89c567be8083d5a9dc0
StellaNadais <126431147+StellaNadais@users.noreply.github.com>
2026-04-01 15:25:36 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ import BookPage from './pages/BookPage'
 import SchedulePage from './pages/SchedulePage'
 import HubPage from './pages/HubPage'
 import TripLogPage from './pages/TripLogPage'
+import WeeklyReceiptPage from './pages/WeeklyReceiptPage'
 import ShiftPage from './pages/ShiftPage'
 import KidJournalPage from './pages/KidJournalPage'
 import InternalNotesPage from './pages/InternalNotesPage'
 import PlaceholderPage from './pages/PlaceholderPage'
+import CaregiverFlowLayout from './layouts/CaregiverFlowLayout'
 import './App.css'
 import './pages/pages.css'
 
@@ -20,11 +22,14 @@ export default function App() {
       <KidJournalProvider>
         <BookingsProvider>
           <Routes>
-            <Route path="/" element={<WelcomePage />} />
+            <Route element={<CaregiverFlowLayout />}>
+              <Route path="/" element={<WelcomePage />} />
+              <Route path="/schedule" element={<SchedulePage />} />
+              <Route path="/hub" element={<HubPage />} />
+            </Route>
             <Route path="/book" element={<BookPage />} />
-            <Route path="/schedule" element={<SchedulePage />} />
-            <Route path="/hub" element={<HubPage />} />
             <Route path="/trip-log" element={<TripLogPage />} />
+            <Route path="/receipt" element={<WeeklyReceiptPage />} />
             <Route path="/shift" element={<ShiftPage />} />
             <Route path="/journal" element={<KidJournalPage />} />
             <Route path="/notes" element={<InternalNotesPage />} />
